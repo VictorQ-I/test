48e366dc8ee7f39d7bfdde0162e19b4c6261f750
VictorQ-I <68793547+VictorQ-I@users.noreply.github.com>
2022-03-26 11:27:14 -0500

    COMPROBACIÓN DE HORA - PARCIAL
    Se compra unas horas determinadas y se hace el test para cada una, en total fueron 5

--- src/test/java/com/tdea/parcial/UtilsTests.java
+++ src/test/java/com/tdea/parcial/UtilsTests.java
@@ -2,9 +2,51 @@ package com.tdea.parcial;
 
 import org.junit.jupiter.api.Test;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
 public class UtilsTests {
 
     @Test
-    public void blankTest() {}
+    public void horaDiaTransformarTextoNight() {
+        int hora = 4;
+
+        String result = Utils.getTimeOfDay(hora);
+        assertEquals("Night", result);
+    }
+
+    @Test
+    public void horaDiaTransformarTextoMorning() {
+        int hora = 8;
+
+        String result = Utils.getTimeOfDay(hora);
+        assertEquals("Morning", result);
+    }
+
+    @Test
+    public void horaDiaTransformarTextoAfternon() {
+        int hora = 16;
+
+        String result = Utils.getTimeOfDay(hora);
+        assertEquals("Afternoon", result);
+    }
+
+    @Test
+    public void horaDiaTransformarTextoEvening() {
+        int hora = 22;
+
+        String result = Utils.getTimeOfDay(hora);
+        assertEquals("Evening", result);
+    }
+
+    @Test
+    public void horaDiaTransformarTextoInvalido() {
+        int hora = 28;
+
+        boolean result = Utils.isValidHour(hora);
+        assertFalse(result);
+    }
 
 }
